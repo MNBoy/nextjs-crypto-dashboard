f449d824a05f0cd324ffbc9c53c42c87dddf433e
MNBoy <moeinsalar7@gmail.com>
2022-03-26 23:07:43 +0430

    Change serverside to static

--- pages/index.jsx
+++ pages/index.jsx
@@ -182,7 +182,7 @@ export default function Home({ coinsData }) {
   );
 }
 
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
   const coinsRes = await fetch(
     'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
   );
